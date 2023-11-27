c78de448679a0151d6fb68d8ec1aad639747a2a2
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-27 10:00:02 +0100

    work to form

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function offer(event) {
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+    let surname = document.getElementById("surname").value;
+    let mail = document.getElementById("mail").value;
+
+    console.log(name)
+}
