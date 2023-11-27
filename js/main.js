1a995d84fd9e40ef6c92626f2f9fcfdfd785ead7
Gabriele002 <gabrieleciarleglio97@gmail.com>
2023-11-27 10:52:53 +0100

    work to discount

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
+
+let sconto = [
+    { codiceSconto: "YHDNU32"},
+    { codiceSconto: "JANJC63"},
+    { codiceSconto: "PWKCN25"},
+    { codiceSconto: "SJDPO96"},
+    { codiceSconto: "POCIE24"},
+]
+
 function offer(event) {
     event.preventDefault();
 
     let name = document.getElementById("name").value;
     let surname = document.getElementById("surname").value;
     let mail = document.getElementById("mail").value;
+    let hours = document.getElementById("hours").value;
 
-    console.log(name)
+    hours = parseFloat(hours);
+
+    if (isNaN(hours)) {
+        alert("Inserisci un carattere valido nella sezione ore")
+    }
+
+    let work = document.getElementById("tipo_di_lavoro").value;
+    work = parseFloat(work)
+
+    let price = work * hours;
+
+    console.log(price)
+    
+
+   
+
+}    
+
+
+
+
+function getWorkPrice(work) {
+    let workPrice = 0;
+    if (work == "Backed") {
+        workPrice = 20.50;
+    }
+    else if (work == "Frontend") {
+        workPrice = 15.30;
+    }
+    else if (work == "Project") {
+        workPrice = 33.60;
+    }
+    return workPrice;
 }
